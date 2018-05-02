d8878df8d25c36fa96667fdddfd36de5e736c8bb
zero <lilu_beautiful@163.com>
2018-05-02 17:25:37 +0800

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -6,4 +6,19 @@ use Tightenco\Collect\Support\Collection;
 
 class Config extends Collection
 {
+  
+  public function get($key, $default = null)
+    {
+        if ($this->offsetExists($key)) {
+            return $this->items[$key];
+        }elseif(trim(strpos($key, "."),".") ){
+        	$keys = explode(".", $key);
+            if($this->offsetExists($keys[0]) && $parentValue = $this->items[$keys[0]]  && isset($this->items[$keys[0]][$keys[1]]) ){
+        	  return ($this->items[$keys[0]][$keys[1]]);
+            }
+        }
+
+        return value($default);
+    }
+  
 }
